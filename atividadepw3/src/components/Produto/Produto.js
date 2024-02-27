afb75fc7c9fe01631a2260cbc21718ca975a7d16
YhanMassato <yhanashihara@gmail.com>
2024-02-26 22:33:58 -0300

    yhan: Estrutura do projeto feita.

--- atividadepw3/src/components/Produto/Produto.js
+++ atividadepw3/src/components/Produto/Produto.js
@@ -1,11 +1,14 @@
 import './Produto.css'
 import Button from '../Botao/Button'
+import Img1 from '../../img/img-placeholder.jpg'
 
-function Produto({image, imgAlt, productName, productType, author}){
+function Produto({image, imgAlt, productName, productType, author, authorLink, description }){
+    const img1 =  Img1;
+
     return(
         <div className="product-main-box">
             <div className="product-left-side">
-                <image className="product-image" src={image} alt={imgAlt} />
+                <image className="product-image" src={image} alt={imgAlt}/>
                 <p className="img-zoom-inst">Passe o mouse para ampliar a imagem</p>
                 <Button/>
                 <div className="divider"/>
@@ -15,10 +18,14 @@ function Produto({image, imgAlt, productName, productType, author}){
                 <h1 className="title-text">{productName} <span className="light-title-text">{productType}</span></h1>
                 <div className="product-author"> 
                     <span>por: </span>
-                    <span><a className="author-link">{author}</a> (autor)</span>
+                    <span><a href={authorLink} className="author-link">{author}</a> (autor)</span>
                     <span className="text-divider">|</span>
-                    <span>Formato:<a className="author-link">{productType}</a></span>                     
+                    <span>Formato:<a className="author-link">{productType}</a></span>
                 </div>{/* product-author */}
+                <div className='divider'/>
+                <div className='product-description'>
+                    <p className='product-description-text'>{description}</p>
+                </div>{/* product-description */}
             </div>{/* product-right-side */}
         </div>/* product-main-box */
     )
