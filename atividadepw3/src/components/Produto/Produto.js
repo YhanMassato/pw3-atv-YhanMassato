85df6ef0e48a03072389a238d0719afe48280879
YhanMassato <yhanashihara@gmail.com>
2024-02-27 22:31:35 -0300

    melhorias do estilo pt2

--- atividadepw3/src/components/Produto/Produto.js
+++ atividadepw3/src/components/Produto/Produto.js
@@ -8,14 +8,16 @@ function Produto({image, imgAlt, productName, productType, author, authorLink, d
     return(
         <div className="product-main-box">
             <div className="product-left-side">
-                <image className="product-image" src={image} alt={imgAlt}/>
+                <div className="image-box">
+                    <image className="product-image" src={image} alt={imgAlt}/>
+                </div>{/* image-box */}
                 <p className="img-zoom-inst">Passe o mouse para ampliar a imagem</p>
                 <Button/>
                 <div className="divider"/>
             </div>{/* product-left-side */}
             
             <div className="product-right-side">
-                <h1 className="title-text">{productName} <span className="light-title-text">{productType}</span></h1>
+                <h1 className="title-text">{productName}LiVRO <span className="light-title-text">{productType}LIVROCINZA</span></h1>
                 <div className="product-author"> 
                     <span>por: </span>
                     <span><a href={authorLink} className="author-link">{author}</a> (autor)</span>
