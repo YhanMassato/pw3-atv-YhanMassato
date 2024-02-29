13043966e69f6adb865fcf0f99f2ada4eaba1e91
YhanMassato <yhanashihara@gmail.com>
2024-02-28 22:31:31 -0300

    Estilização pt 3

--- atividadepw3/src/components/Produto/Produto.js
+++ atividadepw3/src/components/Produto/Produto.js
@@ -6,30 +6,33 @@ function Produto({image, imgAlt, productName, productType, author, authorLink, d
     const img1 =  Img1;
 
     return(
-        <div className="product-main-box">
-            <div className="product-left-side">
-                <div className="image-box">
-                    <image className="product-image" src={image} alt={imgAlt}/>
-                </div>{/* image-box */}
-                <p className="img-zoom-inst">Passe o mouse para ampliar a imagem</p>
-                <Button/>
-                <div className="divider"/>
-            </div>{/* product-left-side */}
+        <div class="product-main-box">
+        <div class="product-left-side">
+            <div class="image-box">
+                <image class="product-image" src="./img-placeholder.jpg" alt="produto-imagem"/>
+            </div> {/*<!--image-box-->*/}
+            <p class="img-zoom-inst">Passe o mouse para ampliar a imagem</p>
             
-            <div className="product-right-side">
-                <h1 className="title-text">{productName}LiVRO <span className="light-title-text">{productType}LIVROCINZA</span></h1>
-                <div className="product-author"> 
-                    <span>por: </span>
-                    <span><a href={authorLink} className="author-link">{author}</a> (autor)</span>
-                    <span className="text-divider">|</span>
-                    <span>Formato:<a className="author-link">{productType}</a></span>
-                </div>{/* product-author */}
-                <div className='divider'/>
-                <div className='product-description'>
-                    <p className='product-description-text'>{description}</p>
-                </div>{/* product-description */}
-            </div>{/* product-right-side */}
-        </div>/* product-main-box */
+            <div class="divider"></div>
+        </div>{/* product-left-side */} 
+        
+        <div class="product-right-side">
+            <h1 class="title-text">LiVRO <span class="light-title-text">LIVROCINZA</span></h1>
+ 
+            <div class="product-author"> 
+                <span>por: </span>
+                <span><a href="" class="author-link">Roberto Rubens</a> (Autor)</span>
+                <span><a href="" class="author-link">Roberta Rubems</a> (Tradutor)</span>
+                <span class="text-divider">|</span>
+                <span>Formato: <a class="author-link">LivroTIPO</a></span>
+            </div>{/* product-author */} 
+            <div class='divider'></div>
+            <div class='product-description'>
+                <p class='product-description-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptates soluta asperiores natus at dolores recusandae, quae veritatis rerum cumque. Cumque culpa dolore voluptas quo sint autem quasi? Sequi, excepturi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptates soluta asperiores natus at dolores recusandae, quae veritatis rerum cumque. Cumque culpa dolore voluptas quo sint autem quasi? Sequi, excepturiLorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptates soluta asperiores natus at dolores recusandae, quae veritatis rerum cumque. Cumque culpa dolore voluptas quo sint autem quasi? Sequi, excepturiLorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptates soluta asperiores natus at dolores recusandae, quae veritatis rerum cumque. Cumque culpa dolore voluptas quo sint autem quasi? Sequi, excepturiLorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptates soluta asperiores natus at dolores recusandae, quae veritatis rerum cumque. Cumque culpa dolore voluptas quo sint autem quasi? Sequi, excepturi
+                </p>
+            </div> {/* product-description */}
+        </div> {/* product-right-side */} 
+    </div> /* product-main-box */
     )
 }
 
