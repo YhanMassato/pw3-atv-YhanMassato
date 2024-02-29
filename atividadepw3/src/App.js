c00cb210a8bae2807b43775b361e2fb3686a7a0b
YhanMassato <yhanashihara@gmail.com>
2024-02-29 19:04:34 -0300

    Yhan: Finalização do CSS

--- atividadepw3/src/App.js
+++ atividadepw3/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Produto from './components/Produto/Produto';
+
+import image from "./img/img-placeholder.jpg";
+
+var desc = "Todos nós conhecemos a Lua. Todos temos uma relação com ela. Todos somos afetados por suas fases. Os povos antigos faziam rituais em sua homenagem, seguiam sua órbita e organizavam o tempo, os feriados, as celebrações e as colheitas de acordo com ela. Mesmo que tenhamos nos distanciado dessa conexão poderosa, os ecos desse sistema ancestral permanecem.Dependendo do momento em que nos encontramos na vida, do que sentimos ou do que está acontecendo ao redor, a Lua pode iluminar caminhos com diversos rituais e hábitos facilmente incorporados ao nosso cotidiano. O livro da Lua é um guia abrangente para uma vida lunar, que inclui ferramentas e recursos e proporciona a estrutura necessária para utilizar o ciclo lunar e estabelecer um relacionamento com nós mesmos."
+
+let livro = image;
+
 function App() {
   return (
     <div className="App">
       <div className="header">  
       </div>
-      <Produto/>
+      <Produto
+        image={livro}
+        imgAlt="Imagem capa Livro da Lua"
+        productName="O Livro da Lua"
+        productType="Livro físico"
+        author="Sarah Feith G."
+        authorLink="/"
+        traslator="Lígia Azevedo"
+        translatorLink="/"
+        description={desc}
+      />
     </div>
   );
 }
--- atividadepw3/src/components/Produto/Produto.js
+++ atividadepw3/src/components/Produto/Produto.js
@@ -1,34 +1,30 @@
 import './Produto.css'
-import Button from '../Botao/Button'
-import Img1 from '../../img/img-placeholder.jpg'
 
-function Produto({image, imgAlt, productName, productType, author, authorLink, description }){
-    const img1 =  Img1;
+function Produto({image, imgAlt, productName, productType, author, authorLink, translator, translatorLink, description }){
 
     return(
         <div class="product-main-box">
         <div class="product-left-side">
             <div class="image-box">
-                <image class="product-image" src="./img-placeholder.jpg" alt="produto-imagem"/>
+                <image class="product-image" src={image} alt={imgAlt}/>
             </div> {/*<!--image-box-->*/}
-            <p class="img-zoom-inst">Passe o mouse para ampliar a imagem</p>
             
             <div class="divider"></div>
         </div>{/* product-left-side */} 
         
         <div class="product-right-side">
-            <h1 class="title-text">LiVRO <span class="light-title-text">LIVROCINZA</span></h1>
+            <h1 class="title-text">{productName} <span class="light-title-text">{productType}</span></h1>
  
             <div class="product-author"> 
                 <span>por: </span>
-                <span><a href="" class="author-link">Roberto Rubens</a> (Autor)</span>
-                <span><a href="" class="author-link">Roberta Rubems</a> (Tradutor)</span>
+                <span><a href={authorLink} class="author-link">{author}</a> (Autor)</span>
+                <span><a href={translatorLink} class="author-link">{translator}</a> (Tradutor)</span>
                 <span class="text-divider">|</span>
-                <span>Formato: <a class="author-link">LivroTIPO</a></span>
+                <span>Formato: <a class="author-link">{productType}</a></span>
             </div>{/* product-author */} 
             <div class='divider'></div>
             <div class='product-description'>
-                <p class='product-description-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptates soluta asperiores natus at dolores recusandae, quae veritatis rerum cumque. Cumque culpa dolore voluptas quo sint autem quasi? Sequi, excepturi.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptates soluta asperiores natus at dolores recusandae, quae veritatis rerum cumque. Cumque culpa dolore voluptas quo sint autem quasi? Sequi, excepturiLorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptates soluta asperiores natus at dolores recusandae, quae veritatis rerum cumque. Cumque culpa dolore voluptas quo sint autem quasi? Sequi, excepturiLorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptates soluta asperiores natus at dolores recusandae, quae veritatis rerum cumque. Cumque culpa dolore voluptas quo sint autem quasi? Sequi, excepturiLorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptates soluta asperiores natus at dolores recusandae, quae veritatis rerum cumque. Cumque culpa dolore voluptas quo sint autem quasi? Sequi, excepturi
+                <p class='product-description-text'> {description}
                 </p>
             </div> {/* product-description */}
         </div> {/* product-right-side */} 
